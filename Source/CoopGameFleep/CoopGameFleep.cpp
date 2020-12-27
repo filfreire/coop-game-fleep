a7503421038ed0a8def5ad9c66aa786e374baf6d
Filipe Freire <livrofubia@gmail.com>
2020-12-27 17:56:25 +0100

    initial commit

---
+++ Source/CoopGameFleep/CoopGameFleep.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoopGameFleep.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoopGameFleep, "CoopGameFleep" );
---
+++ Source/CoopGameFleep/CoopGameFleepGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopGameFleepGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPGAMEFLEEP_API ACoopGameFleepGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
