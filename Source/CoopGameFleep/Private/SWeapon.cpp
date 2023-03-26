2b5673afa5d56521414737bdf5eddaca8210adb6
Filipe Freire <livrofubia@gmail.com>
2023-03-26 21:58:05 +0100

    small debug change

--- Source/CoopGameFleep/Private/SWeapon.cpp
+++ Source/CoopGameFleep/Private/SWeapon.cpp
@@ -8,6 +8,9 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Particles/ParticleSystemComponent.h"
 
+static int32 DebugWeaponDrawing = 0;
+FAutoConsoleVariableRef CVarDebugWeapon(TEXT("COOP.DebugWeapons"), DebugWeaponDrawing, TEXT("Draw Debug Lines for Weapons"), ECVF_Cheat);
+
 // Sets default values
 ASWeapon::ASWeapon()
 {
@@ -70,7 +73,10 @@ void ASWeapon::Fire()
 			TracerEndpoint = Hit.ImpactPoint;
 		}
 
-		//DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+		if (DebugWeaponDrawing != 0) {
+			DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+		}
+		
 
 		if (MuzzleEffect)
 		{
