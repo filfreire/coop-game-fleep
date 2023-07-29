3b0d166cd4f962de98cc9f40c19a5e6e0183e15d
Filipe Freire <livrofubia@gmail.com>
2023-07-29 14:41:54 +0100

    remove duplicate build

--- scripts/Build.bat
+++ scripts/Build.bat
@@ -2,4 +2,4 @@ set ueLocation=%~1
 set projectLocation=%~2
 set projectName=%~3
 
-"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -noP4 -platform=Win64 -clientconfig=Development -build
+"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -noP4 -platform=Win64  -archivedirectory="%projectLocation%\%packageFolder%" -clientconfig=Development -build -archive
