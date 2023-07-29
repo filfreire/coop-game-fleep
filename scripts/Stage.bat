261f526cbe7f9d433549b4c448f97bf784852ca3
Filipe Freire <livrofubia@gmail.com>
2023-07-29 14:40:07 +0100

    revert back to previous working point

--- scripts/Stage.bat
+++
@@ -1,5 +0,0 @@
-set ueLocation=%~1
-set projectLocation=%~2
-set projectName=%~3
-
-"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -noP4 -platform=Win64 -clientconfig=Development -archivedirectory="%projectLocation%\%packageFolder%" -stage -skipcook -makebinaryconfig
