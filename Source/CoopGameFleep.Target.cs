ba9264c3c008c5cf75f39a41e8995d90f53aa6e0
Filipe Freire <livrofubia@gmail.com>
2023-08-01 00:03:43 +0100

    Improve CI pipeline (#14)
    * add clean script and add IncludeOrderVersion
    
    * Disable livecoding on build
    
    * Rebuild lighting step
    
    * setup editor frame rate
    
    * add cleanup step

--- Source/CoopGameFleep.Target.cs
+++ Source/CoopGameFleep.Target.cs
@@ -10,5 +10,6 @@ public class CoopGameFleepTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "CoopGameFleep" } );
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 	}
 }
