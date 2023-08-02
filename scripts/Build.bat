6b229d4df9f48b6751b1f0842dfd3e027413a4ae
Filipe Freire <livrofubia@gmail.com>
2023-08-02 01:03:53 +0100

    Revert "split cook and rm duplicate build inside Package.bat"
    This reverts commit fa37823788598348e0a5f7c7e2835da0f86c6e24.

--- scripts/Build.bat
+++ scripts/Build.bat
@@ -2,4 +2,4 @@ set ueLocation=%~1
 set projectLocation=%~2
 set projectName=%~3
 
-"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -noP4 -platform=Win64 -clientconfig=Development -build -skipcook
+"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -noP4 -platform=Win64 -clientconfig=Development -build
--- scripts/Cook.bat
+++
@@ -1,5 +0,0 @@
-set ueLocation=%~1
-set projectLocation=%~2
-set projectName=%~3
-
-"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -nop4 -utf8output -cook -stage
